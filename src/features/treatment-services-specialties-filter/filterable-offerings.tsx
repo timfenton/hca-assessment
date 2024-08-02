5b76c140bbcc1807499684410c0b83e1456f84d7
timfenton-shaw <79111986+timfenton-shaw@users.noreply.github.com>
2024-08-02 16:23:36 -0400

    change back to local data.json

--- src/features/treatment-services-specialties-filter/filterable-offerings.tsx
+++ src/features/treatment-services-specialties-filter/filterable-offerings.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
+import data from './data/data.json';
 import HCAButtonSecondary from '../../components/shared/buttons/hca-btn-secondary';
 
 interface Offering {
@@ -12,57 +13,39 @@ interface GroupedOfferings {
 
 const FilterableOfferings: React.FC = (): React.ReactElement => {
   const [filter, setFilter] = useState<string | null>(null);
-  const [data, setData] = useState<{termList: Offering[]} | null>(null);
-
-  useEffect(() => {
-    fetch('/data/data.json')
-      .then(response => response.json())
-      .then(data => {
-        if (!data) throw new Error('Bad data!');
-        setData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching the JSON:', error);
-        throw error;
-      });
-  }, []);
 
   const groupedData = useMemo(() => {
-    if (!data) return {};
-
     const sortedTerms = [...data.termList].sort((a, b) =>
-      a.title.localeCompare(b.title)
-    );
+        a.title.localeCompare(b.title)
+      );
 
-    const grouped: GroupedOfferings = sortedTerms.reduce(
-      (acc: GroupedOfferings, item: Offering) => {
-        let firstChar = item.title[0];
+      const grouped: GroupedOfferings = sortedTerms.reduce(
+        (acc: GroupedOfferings, item: Offering) => {
+          let firstChar = item.title[0];
 
-        if (/^[A-Za-z]$/.test(firstChar)) {
-          firstChar = firstChar.toUpperCase();
-        } else {
-          firstChar = '#';
-        }
+          if (/^[A-Za-z]$/.test(firstChar)) {
+            firstChar = firstChar.toUpperCase();
+          } else {
+            firstChar = '#';
+          }
 
-        if (!acc[firstChar]) {
-          acc[firstChar] = [];
-        }
+          if (!acc[firstChar]) {
+            acc[firstChar] = [];
+          }
 
-        acc[firstChar].push(item);
+          acc[firstChar].push(item);
 
-        return acc;
-      },
-      {}
-    );
-
-    return grouped;
-  }, [data]);
+          return acc;
+        },
+        {}
+      );
+  
+      return grouped;
+  }, []);
 
   const filteredData = filter
     ? { [filter]: groupedData[filter] }
     : groupedData;
-
-  if (!data) return <>Loading...</>;
 
   return (
     <div className="flex flex-col space-y-5 items-center bg-white">
@@ -82,29 +65,29 @@ const FilterableOfferings: React.FC = (): React.ReactElement => {
       </div>
       <div className="flex w-full bg-gray-100 items-center justify-center">
         <div className="flex flex-col w-11/12 sm:w-1/2 bg-white p-5 shadow-xl mt-5 mb-10">
-          {filter && 
-            <div className="flex justify-between items-right mb-5 w-full">
-              <HCAButtonSecondary
-                className="w-20 rounded-sm"
-                ariaLabel="Reset filter"
-                buttonAction={() => setFilter(null)}
-              >
-                Reset
-              </HCAButtonSecondary>
-            </div>}
-          {Object.entries(filteredData).map(([letter, items]) => (
-            <div key={letter} className='mb-5'>
-              <h2 className='underline underline-offset-4 mb-3'>{letter}&nbsp;&nbsp;&nbsp;</h2>
-              
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                {items.map((item) => (
-                  <a key={item.title} href={item.link} className="text-hca-link-active hover:underline">
-                    {item.title}
-                  </a>
+            {filter && 
+                <div className="flex justify-between items-right mb-5 w-full">
+                    <HCAButtonSecondary
+                        className="w-20 rounded-sm"
+                        ariaLabel="Reset filter"
+                        buttonAction={() => setFilter(null)}
+                    >
+                        Reset
+                    </HCAButtonSecondary>
+                </div>}
+            {Object.entries(filteredData).map(([letter, items]) => (
+                <div key={letter} className='mb-5'>
+                    <h2 className='underline underline-offset-4 mb-3'>{letter}&nbsp;&nbsp;&nbsp;</h2>
+                    
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        {items.map((item) => (
+                            <a key={item.title} href={item.link} className="text-hca-link-active hover:underline">
+                            {item.title}
+                            </a>
+                        ))}
+                    </div>
+                </div>
                 ))}
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </div>
